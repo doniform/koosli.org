0d26047adbee669d95f15195ca05ad525b324007
Tarjei Husøy <git@thusoy.com>
2014-09-30 17:06:35 +0200

    Add fabric deployments

---
+++ tools/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Fail script if any command fails
+set -e
+
+# Push code coverage to gh-pages
+./tools/coverage_to_gh_pages.sh
+
+# Decrypt secrets
+./tools/secure_data.py decrypt -k $SALT_SECRET
+
+# Configure SSH keys for travis-ci user
+python -c "import yaml; fh = open('pillar/secure/init.sls'); d = yaml.load(fh); print d['TRAVIS_SSH_PRIVATE_KEY']" > ~/.ssh/id_rsa
+
+# Run provisioning on server and deploy code and static assets to server
+fab provision deploy -H travis@koosli.org:21890
