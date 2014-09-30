8ca52f131f93880d6389fcccb910b755c94bb899
Tarjei Husøy <git@thusoy.com>
2014-10-01 01:50:31 +0200

    Add db config to koosli state

--- salt/koosli/prod_settings.py
+++ salt/koosli/prod_settings.py
@@ -1,3 +1,6 @@
 {% set koosli = pillar.get('koosli') -%}
+{% set postgres = pillar.get('postgres') -%}
 
 SECRET_KEY = '{{ koosli.secret_key }}'
+
+SQLALCHEMY_DATABASE_URL = "postgresql://koosli:{{ koosli['db_password'] }}@{{ postgres['db_url'] }}/koosli_rel"
