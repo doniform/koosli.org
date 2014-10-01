7aa2ac8f8eb7bcf8e76d35a5b24a77ece2ed3583
Tarjei Husøy <git@thusoy.com>
2014-10-01 14:17:51 +0200

    Add SEARCH_PROVIDERS to prod_settings

--- salt/koosli/prod_settings.py
+++ salt/koosli/prod_settings.py
@@ -1,6 +1,12 @@
 {% set koosli = pillar.get('koosli') -%}
 {% set postgres = pillar.get('postgres') -%}
 
+from koosli.search_providers import bing
+
 SECRET_KEY = "{{ pillar['KOOSLI_SECRET_KEY'] }}"
 
 SQLALCHEMY_DATABASE_URL = "postgresql://koosli:{{ pillar['KOOSLI_DB_PASSWORD'] }}@{{ postgres['db_url'] }}/koosli_rel"
+
+SEARCH_PROVIDERS = {
+    'bing': bing.Bing,
+}
