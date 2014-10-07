a3e054e7cac9036b69cde4dd214f67ab5d1f3deb
Tarjei Husøy <git@thusoy.com>
2014-10-07 23:37:25 +0200

    Add log conf

--- salt/koosli/prod_settings.py
+++ salt/koosli/prod_settings.py
@@ -1,14 +1,15 @@
 {% set koosli = pillar.get('koosli') -%}
-{% set postgres = pillar.get('postgres') -%}
 
 from koosli.search_providers import bing
 
 SECRET_KEY = "{{ pillar['KOOSLI_SECRET_KEY'] }}"
 
-SQLALCHEMY_DATABASE_URL = "postgresql://koosli:{{ pillar['KOOSLI_DB_PASSWORD'] }}@{{ postgres['db_url'] }}/koosli_rel"
+SQLALCHEMY_DATABASE_URL = "postgresql://koosli:{{ pillar['KOOSLI_DB_PASSWORD'] }}@{{ salt['config.get']('postgres.host', 'localhost') }}/koosli_rel"
 
 SEARCH_PROVIDERS = {
     'bing': bing.Bing,
 }
 
 BING_API_KEY = "{{ pillar['BING_API_KEY'] }}"
+
+LOG_CONF_PATH = '/srv/koosli.org/log_conf.yaml'
